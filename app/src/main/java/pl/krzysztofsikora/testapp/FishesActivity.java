249bb5ee89917e2c2fecfcc3d6d06709e31630b7
krzysztof <listdlakrzycha@gmail.com>
2017-03-03 20:28:43 +0100

    load file and scroll view

--- app/src/main/java/pl/krzysztofsikora/testapp/FishesActivity.java
+++ app/src/main/java/pl/krzysztofsikora/testapp/FishesActivity.java
@@ -1,13 +1,32 @@
 package pl.krzysztofsikora.testapp;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class FishesActivity extends AppCompatActivity {
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class FishesActivity extends AppCompatActivity implements CommonColors {
+
+
+    private TextView fiches;
+    private String path = Environment.getExternalStorageDirectory().toString() + "/DigitalZombieLab/Druga/";
+    private String interlude = "\n\n\n///---///----////\n\n\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +34,46 @@ public class FishesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fiches);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setNaviBarColor();
+        fiches = (TextView) findViewById(R.id.fichesTextView);
+        fiches.setText(getAllContent());
     }
+
+    @Override
+    public void setNaviBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+    private String getAllContent() {
+
+        try {
+            File file = new File(path);
+            String[] paths = file.list(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".txt");
+                }
+            });
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < paths.length; i++) {
+
+
+                String str = FileUtils.readFileToString(new File(path + paths[i]), "UTF-8");
+
+                sb.append(str);
+                sb.append(interlude);
+            }
+            return sb.toString();
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Bug" + e.toString(), Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
 
 }
