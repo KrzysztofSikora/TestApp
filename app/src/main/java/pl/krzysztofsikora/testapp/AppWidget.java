8d1599747ff32354174f022208640ebde01f584f
krzysztof <listdlakrzycha@gmail.com>
2017-03-07 15:01:53 +0100

    load file and scroll view

--- app/src/main/java/pl/krzysztofsikora/testapp/AppWidget.java
+++ app/src/main/java/pl/krzysztofsikora/testapp/AppWidget.java
@@ -1,9 +1,13 @@
 package pl.krzysztofsikora.testapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import java.util.Random;
 
 /**
  * Implementation of App Widget functionality.
@@ -11,14 +15,22 @@ import android.widget.RemoteViews;
 public class AppWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                int appWidgetId, int[] appWidgetIds) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // TODO load file
+        // CharSequence widgetText = context.getString(R.string.appwidget_text);
+        String number = String.valueOf(new Random().nextInt());
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text, number);
 
         // Instruct the widget manager to update the widget
+
+        Intent intent = new Intent(context, AppWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.imageIco, pendingIntent);
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -26,7 +38,7 @@ public class AppWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId, appWidgetIds);
         }
     }
 
